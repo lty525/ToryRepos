39de57db4ad37d84f0a12589d73ebdf3059d53c7
Tory <tory>
2018-11-05 21:08:41 +0800

    这是我的本地第一个本地工程添加到git

---
+++ MyTestGit/src/cn/itcast/MyTestGit.java
@@ -0,0 +1,19 @@
+package cn.itcast;
+
+/**
+ * @Description: java类作用描述
+ * @Author: Lty
+ * @CreateDate: 2018/11/5$ 21:05$
+ * @Version: 1.0
+ */
+
+public class MyTestGit {
+    public static void main(String[] args) {
+        System.out.println("222222222");
+        System.out.println("222222222");
+        System.out.println("222222222");
+        System.out.println("222222222");
+        System.out.println("222222222");
+    }
+}
+
